af90cc280ae644b03d2f7b78eef10d5f460ffaac
Ope2b <rakzeiqo@live.fr>
2021-09-23 16:01:34 +0200

    9. Background Synchronisation - résultat

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const technosDiv = document.querySelector('#technos');
 function loadTechnologies(technos) {
-    fetch('http://localhost:3001/technos')
+    fetch('https://us-central1-pwa-technos-giancoli.cloudfunctions.net/getTechnos')
         .then(response => {
             response.json()
                 .then(technos => {
